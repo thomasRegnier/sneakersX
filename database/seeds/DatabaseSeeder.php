53a333bb8b40e38560bb5b0ef4c610a21e56361f
thomasRegnier <thomas.regnier3001@gmail.com>
2020-06-19 14:22:33 +0200

    change path ime

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        $this->call(BrandTableSeeder::class);
-        $this->call(ProductTableSeeder::class);
+      //  $this->call(BrandTableSeeder::class);
+      //  $this->call(ProductTableSeeder::class);
     }
 }
